ab2ea2def901cd4dcea2b44fb85f36d7a8393117
pychangeric <pychangeric@gmail.com>
2023-06-09 00:53:57 +0300

    revision

--- src/components/home.js
+++ src/components/home.js
@@ -12,9 +12,10 @@ const Home = () => {
     author: '',
     amount: '',
     genre: '',
+    image_url: ''
   });
-  const [searchResults, setSearchResults] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isEditMode, setIsEditMode] = useState(false);
 
   useEffect(() => {
     fetchData();
@@ -32,7 +33,7 @@ const Home = () => {
 
   const createBook = async () => {
     try {
-      const response = await fetch('http://localhost:9292/', {
+      const response = await fetch('http://localhost:9292/books', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -47,15 +48,19 @@ const Home = () => {
         author: '',
         amount: '',
         genre: '',
+        image_url: ''
       });
+      closeModal();
     } catch (error) {
       console.error('Error creating book:', error);
     }
   };
 
+  
+
   const deleteBook = async (bookId) => {
     try {
-      await fetch(`http://localhost:9292/${bookId}`, {
+      await fetch(`http://localhost:9292/books/${bookId}`, {
         method: 'DELETE',
       });
       setData(data.filter((book) => book.id !== bookId));
@@ -66,10 +71,19 @@ const Home = () => {
 
   const openModal = (book) => {
     setSelectedItem(book);
+    setIsEditMode(true);
+    setIsModalOpen(true);
+  };
+
+  const openAddModal = () => {
+    setIsEditMode(false);
+    setIsModalOpen(true);
   };
 
   const closeModal = () => {
     setSelectedItem(null);
+    setIsEditMode(false);
+    setIsModalOpen(false);
   };
 
   const handleInputChange = (event) => {
@@ -80,114 +94,112 @@ const Home = () => {
     }));
   };
 
-  const handleSearch = (event) => {
-    event.preventDefault();
-    const searchValue = event.target.elements.search.value;
-
-    // Perform search logic here
-    const filteredData = data.filter((item) => {
-      // Assuming you want to search by book title
-      return item.title.toLowerCase().includes(searchValue.toLowerCase());
-    });
-
-    setSearchResults(filteredData);
-  };
-
   return (
     <div className="Home">
       <NavBar />
-      <h1>The Novels</h1>
+      <h1>Bookstore</h1>
 
-      <div className="row2">
-        <h2>Find Your Book</h2>
-        <div className="search">
-          <form onSubmit={handleSearch}>
-            <input type="text" name="search" placeholder="Enter Book Name" />
-            <button type="submit">Search</button>
-          </form>
-        </div>
+      <div className="search">
+        <form>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={newBook.title}
+            onChange={handleInputChange}
+          />
+          <button type="button" onClick={openAddModal}>
+            Add Book
+          </button>
+        </form>
       </div>
 
       <ul>
-        {searchResults.length > 0
-          ? searchResults.map((item) => (
-              <li key={item.id}>
-                <button onClick={() => openModal(item)}>
-                  <img src={item.image_url} alt="Book cover" />
-                </button>
-                <button onClick={() => deleteBook(item.id)}>Delete</button>
-              </li>
-            ))
-          : data.map((item) => (
-              <li key={item.id}>
-                <button onClick={() => openModal(item)}>
-                  <img src={item.image_url} alt="Book cover" />
-                </button>
-                <button onClick={() => deleteBook(item.id)}>Delete</button>
-              </li>
-            ))}
+        {data.map((item) => (
+          <li key={item.id}>
+            <button onClick={() => openModal(item)}>
+              <img src={item.image_url} alt="Book cover" />
+            </button>
+            <button onClick={() => deleteBook(item.id)}>Delete</button>
+          </li>
+        ))}
       </ul>
-
-      <button type="button" onClick={() => setIsModalOpen(true)}>
-        Create a New Book
-      </button>
 
       {isModalOpen && (
         <Modal
           isOpen={true}
           onRequestClose={closeModal}
-          contentLabel="Create a New Book"
+          contentLabel="Create or Edit Book"
         >
-          <h2>Create a New Book</h2>
+          <h2>{isEditMode ? 'Edit Book' : 'Create a New Book'}</h2>
           <form>
-            <label>Title:</label>
+            <label htmlFor="title">Title:</label>
             <input
               type="text"
+              id="title"
               name="title"
               value={newBook.title}
               onChange={handleInputChange}
             />
             <br />
 
-            <label>Description:</label>
+            <label htmlFor="description">Description:</label>
             <input
               type="text"
+              id="description"
               name="description"
               value={newBook.description}
               onChange={handleInputChange}
             />
             <br />
 
-            <label>Author:</label>
+            <label htmlFor="author">Author:</label>
             <input
               type="text"
+              id="author"
               name="author"
               value={newBook.author}
               onChange={handleInputChange}
             />
             <br />
 
-            <label>Amount:</label>
+            <label htmlFor="amount">Amount:</label>
             <input
               type="text"
+              id="amount"
               name="amount"
               value={newBook.amount}
               onChange={handleInputChange}
             />
             <br />
 
-            <label>Genre:</label>
+            <label htmlFor="genre">Genre:</label>
             <input
               type="text"
+              id="genre"
               name="genre"
               value={newBook.genre}
               onChange={handleInputChange}
             />
             <br />
 
-            <button type="submit" onClick={createBook}>
-              Submit
-            </button>
+            <label htmlFor="image_url">Image URL:</label>
+            <input
+              type="text"
+              id="image_url"
+              name="image_url"
+              value={newBook.image_url}
+              onChange={handleInputChange}
+            />
+            <br />
+
+            {!isEditMode && (
+               <button type="button" onClick={createBook}>
+                 Create
+               </button>
+            )}
+
             <button type="button" onClick={closeModal}>
               Cancel
             </button>
@@ -203,9 +215,7 @@ const Home = () => {
         >
           <h2>Title: {selectedItem.title}</h2>
           <p>Description: {selectedItem.description}</p>
-          <p>Author: {selectedItem.author}</p>
-          <p>Amount: {selectedItem.amount}</p>
-          <p>Genre: {selectedItem.genre}</p>
+          <button onClick={() => setIsEditMode(true)}>Edit</button>
           <button onClick={closeModal}>Close</button>
         </Modal>
       )}
